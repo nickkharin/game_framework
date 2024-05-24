534e915b50be72cd98ef4249712dcbda3fb13b0f
Nikolay Kharin <nick-kharin@mail.ru>
2024-05-25 01:50:23 +0400

    Update test_start_game.py

--- tests/test_start_game.py
+++ tests/test_start_game.py
@@ -1,5 +1,6 @@
 import pytest
 import allure
+from utils.locators import GardenscapesLocators
 
 @pytest.mark.usefixtures("game_actions")
 class TestGardenscapes:
@@ -8,41 +9,35 @@ class TestGardenscapes:
     @allure.story('Launch the game and wait for Play button')
     @allure.step('Check if Play button is present after launching the game')
     def test_launch_game(self, game_actions):
-        play_button_id = 'com.playrix.gardenscapes:id/play_button'
-        game_actions.wait_for_element(play_button_id)
-        assert game_actions.is_element_present(play_button_id), "Play button is not present after launching the game."
+        game_actions.wait_for_element(GardenscapesLocators.PLAY_BUTTON_XPATH)
+        assert game_actions.is_element_present(GardenscapesLocators.PLAY_BUTTON_XPATH), "Play button is not present after launching the game."
 
     @allure.feature('Click Play Button')
     @allure.story('Click the Play button')
     @allure.step('Click Play button and verify it is no longer present')
     def test_click_play_button(self, game_actions):
-        play_button_id = 'com.playrix.gardenscapes:id/play_button'
-        game_actions.click_element(play_button_id)
-        assert not game_actions.is_element_present(play_button_id), "Play button is still present after clicking it."
+        game_actions.click_element(GardenscapesLocators.PLAY_BUTTON_XPATH)
+        assert not game_actions.is_element_present(GardenscapesLocators.PLAY_BUTTON_XPATH), "Play button is still present after clicking it."
 
     @allure.feature('Game Opened')
     @allure.story('Verify the game opened after clicking Play button')
     @allure.step('Verify game screen is present after clicking Play button')
     def test_game_opened(self, game_actions):
-        game_screen_id = 'com.playrix.gardenscapes:id/game_screen'
-        assert game_actions.is_element_present(game_screen_id), "Game screen did not open after clicking Play button."
+        assert game_actions.is_element_present(GardenscapesLocators.GAME_SCREEN_XPATH), "Game screen did not open after clicking Play button."
 
     @allure.feature('End-to-End Test')
     @allure.story('Complete end-to-end test')
     @allure.step('Perform end-to-end test from launching the game to verifying game screen')
     def test_end_to_end(self, game_actions):
-        play_button_id = 'com.playrix.gardenscapes:id/play_button'
-        game_screen_id = 'com.playrix.gardenscapes:id/game_screen'
-        
         with allure.step('Launch the game and wait for Play button'):
-            game_actions.wait_for_element(play_button_id)
-            assert game_actions.is_element_present(play_button_id), "Play button is not present after launching the game."
+            game_actions.wait_for_element(GardenscapesLocators.PLAY_BUTTON_XPATH)
+            assert game_actions.is_element_present(GardenscapesLocators.PLAY_BUTTON_XPATH), "Play button is not present after launching the game."
             allure.attach(game_actions.driver.get_screenshot_as_png(), name="Launch Game", attachment_type=allure.attachment_type.PNG)
 
         with allure.step('Click Play button'):
-            game_actions.click_element(play_button_id)
+            game_actions.click_element(GardenscapesLocators.PLAY_BUTTON_XPATH)
             allure.attach(game_actions.driver.get_screenshot_as_png(), name="Click Play Button", attachment_type=allure.attachment_type.PNG)
 
         with allure.step('Verify the game screen is present'):
-            assert game_actions.is_element_present(game_screen_id), "Game screen did not open after clicking Play button."
+            assert game_actions.is_element_present(GardenscapesLocators.GAME_SCREEN_XPATH), "Game screen did not open after clicking Play button."
             allure.attach(game_actions.driver.get_screenshot_as_png(), name="Game Screen", attachment_type=allure.attachment_type.PNG)
