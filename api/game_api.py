9a4144250649dd6b24a3d3ec552c3cb769679c71
Nikolay Kharin <nick-kharin@mail.ru>
2024-05-25 01:39:50 +0400

    Update game_api.py

--- api/game_api.py
+++ api/game_api.py
@@ -4,9 +4,12 @@ class GameAPI:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def send_command(self, command, params=None):
+    def send_command(self, command, params=None, method='POST'):
         url = f"{self.base_url}/{command}"
-        response = requests.post(url, json=params)
+        if method.upper() == 'PUT':
+            response = requests.put(url, json=params)
+        else:
+            response = requests.post(url, json=params)
         return response.json()
 
     def get_button_coordinates(self, button_id):
@@ -14,3 +17,10 @@ class GameAPI:
 
     def pass_level(self, start_level, end_level):
         return self.send_command('pass_level', {'start_level': start_level, 'end_level': end_level})
+
+    def click_element(self, element_id):
+        return self.send_command('click_element', {'element_id': element_id})
+
+    def open_window(self, window_id):
+        return self.send_command('open_window', {'window_id': window_id})
+        
